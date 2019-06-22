d73197a9a1243b2b6d0ddf7813b287e9bbdf1d72
Ivan Enzhaev <8observer8@gmail.com>
2019-06-23 00:41:24 +0400

    Deleted the project 001_hello-canvas - Copy

--- glMatrixTypeScript/ch02/001_hello-canvas - Copy/src/RequireConfig.ts
+++
@@ -1,8 +0,0 @@
-requirejs.config({
-    baseUrl: "dist",
-    paths: {
-        "gl-matrix": "https://cdnjs.cloudflare.com/ajax/libs/gl-matrix/2.8.1/gl-matrix-min"
-    }
-});
-
-requirejs(["Program"], (Program) => { });
